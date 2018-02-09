a48f44702a5deae8c50653fe2bde693b9cc164f3
Andrew Pagan <apagan1@umbc.edu>
2018-02-08 22:13:31 -0500

    Now trains and compares actual value to predicted value

--- network.py
+++ network.py
@@ -1,43 +1,129 @@
-import sys
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QDesktopWidget, QWidget
-from PyQt5.QtGui import QIcon
+import numpy
+import math
+import matplotlib.pyplot
+import scipy.special
+import time
 
-class mainWindow(QMainWindow):
-    
-	def __init__(self):
-		super().__init__()
-        
-		self.initUI()
-        
-        
-	def initUI(self):
+#Neural network definition
+class neuralNetwork:
+	def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+		self.inodes = inputnodes
+		self.hnodes = hiddennodes
+		self.onodes = outputnodes
+		self.lrate = learningrate
 
-		exitAct = QAction(QIcon('exit.png'), '&Exit', self)
-		exitAct.setShortcut('Ctrl+Q')
-		exitAct.setStatusTip('Exit Application')
-		exitAct.triggered.connect(qApp.quit)
+		self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+		self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
 
-		self.resize(640, 480)
-		self.statusBar().showMessage('Ready')
-		self.center()
-		self.setWindowTitle('Neural Network') 
-		self.statusBar()  
+		#Activation function
+		self.activation_function = lambda x: scipy.special.expit(x)
 
-		menubar = self.menuBar()
-		menubar.setNativeMenuBar(False)
-		fileMenu = menubar.addMenu('&File')
-		fileMenu.addAction(exitAct)
 
-		self.show()
+	def train(self, inputs_list, targets_list):
+		inputs = numpy.array(inputs_list, ndmin=2).T
+		targets = numpy.array(targets_list, ndmin=2).T
 
-	def center(self):
-		qr = self.frameGeometry()
-		cp = QDesktopWidget().availableGeometry().center()
-		qr.moveCenter(cp)
-		self.move(qr.topLeft())
-        
-if __name__ == '__main__':
-    
-	app = QApplication(sys.argv)
-	ex = mainWindow()
-	sys.exit(app.exec_())
+		hidden_inputs = numpy.dot(self.wih, inputs)
+		hidden_outputs = self.activation_function(hidden_inputs)
+
+		final_inputs = numpy.dot(self.who, hidden_outputs)
+		final_outputs = self.activation_function(final_inputs)
+		#Error (target-actual)
+		output_errors = targets - final_outputs
+
+		hidden_errors = numpy.dot(self.who.T, output_errors)
+
+		self.who += self.lrate * numpy.dot((output_errors * final_outputs * (1.0-final_outputs)), numpy.transpose(hidden_outputs))
+		self.wih += self.lrate * numpy.dot((hidden_errors * hidden_outputs * (1.0-hidden_outputs)), numpy.transpose(inputs))
+
+	def query(self, inputs_list):
+
+		#Convert inputs list to 2d array
+		inputs = numpy.array(inputs_list, ndmin=2).T
+
+		#Calculate signals into hidden layer
+		hidden_inputs = numpy.dot(self.wih, inputs)
+		hidden_outputs = self.activation_function(hidden_inputs)
+
+		#Calculate signals into final output later
+		final_inputs = numpy.dot(self.who, hidden_outputs)
+
+		#Calculate the signals emerging from the final layer
+		final_outputs = self.activation_function(final_inputs)
+
+		return final_outputs
+
+#Declare neural network parameters
+input_nodes = 784
+hidden_nodes = 100
+output_nodes = 10
+learning_rate = 0.3
+
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
+training_data_file = open("mnist_train.csv", 'r')
+training_data_list = training_data_file.readlines()
+training_data_file.close()
+
+#Train the neural network
+#Epochs is the number of times the training data set is used for training
+epochs = 5
+
+print("Training...")
+for e in range(epochs):
+    #Go through all records in the training data set
+    for record in training_data_list:
+        #Split the record by the ',' commas
+        all_values = record.split(',')
+        #Scale and shift the inputs
+        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+        #Create the target output values (all 0.01, except the desired label which is 0.99)
+        targets = numpy.zeros(output_nodes) + 0.01
+        #All_values[0] is the target label for this record
+        targets[int(all_values[0])] = 0.99
+        n.train(inputs, targets)
+        pass
+    pass
+
+print("Done training...")
+
+#Load the mnist test data CSV file into a list
+test_data_file = open("mnist_test.csv", 'r')
+test_data_list = test_data_file.readlines()
+test_data_file.close()
+
+#Scorecard for how well the network performs, initially empty
+scorecard = []
+num = 0
+print("Testing data...")
+#Go through all the records in the test data set
+for record in test_data_list:
+    #Split the record by the ',' commas
+	all_values = record.split(',')
+    #Correct answer is first value
+	correct_label = int(all_values[0])
+    #Scale and shift the inputs
+	inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+    #Query the network
+	outputs = n.query(inputs)
+    #The index of the highest value corresponds to the label
+	label = numpy.argmax(outputs)
+	print("Network's answer:", label)
+	print("Correct label:", correct_label)
+    #Append correct or incorrect to list
+	if (label == correct_label):
+        #Network's answer matches correct answer, add 1 to scorecard
+		scorecard.append(1)
+	else:
+        #Network's answer doesn't match correct answer, add 0 to scorecard
+		scorecard.append(0)
+		pass
+
+	#Display correct letter
+	all_values = test_data_list[num].split(',')
+	image_array= numpy.asfarray(all_values[1:]).reshape((28,28))
+	matplotlib.pyplot.imshow(image_array, cmap='Greys', interpolation='None')
+	matplotlib.pyplot.show()
+	time.sleep(1.0)
+	num = num+1
+	pass
