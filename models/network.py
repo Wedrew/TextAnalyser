db1430b745d724093a0fa53273b56ca0193170d5
Andrew Pagan <apagan1@umbc.edu>
2018-03-11 18:28:21 -0400

    Remove dictionary

--- models/network.py
+++ models/network.py
@@ -12,59 +12,6 @@ import pyopencl as cl
 import pyopencl.array as pycl_array
 from src.helper import *
 
-#Mapping for emnist training data, does not include some letters which may have similar
-#lower and upper case for better performance
-#Converting from decimal to ascii is done in epoch function
-emnistMapping = {
-    0:'0',
-    1:'1',
-    2:'2',
-    3:'3',
-    4:'4',
-    5:'5',
-    6:'6',
-    7:'7',
-    8:'8',
-    9:'9',
-    10:'A',
-    11:'B',
-    12:'C',
-    13:'D',
-    14:'E',
-    15:'F',
-    16:'G',
-    17:'H',
-    18:'I',
-    19:'J',
-    20:'K',
-    21:'L',
-    22:'M',
-    23:'N',
-    24:'O',
-    25:'P',
-    26:'Q',
-    27:'R',
-    28:'S',
-    29:'T',
-    30:'U',
-    31:'V',
-    32:'W',
-    33:'X',
-    34:'Y',
-    35:'Z',
-    36:'a',
-    37:'b',
-    38:'d',
-    39:'e',
-    40:'f',
-    41:'g',
-    42:'h',
-    43:'n',
-    44:'q',
-    45:'r',
-    46:'t',
-}
-
 #Neural network definition
 class NeuralNetwork:
     def __init__(self, inputNodes=1, hiddenNodes=1, outputNodes=1, learningRate=0.0):
@@ -181,9 +128,9 @@ class NeuralNetwork:
         label = emnistMapping[numpy.argmax(outputs)]
         #Calculate certainty
         certainty = self.softmax(self.softmaxOutputs)
-        imageArray= numpy.asfarray(image[:]).reshape((28,28))
-        matplotlib.pyplot.imshow(imageArray, cmap='Greys', interpolation='None')
-        matplotlib.pyplot.show()
+        # imageArray= numpy.asfarray(image[:]).reshape((28,28))
+        # matplotlib.pyplot.imshow(imageArray, cmap='Greys', interpolation='None')
+        # matplotlib.pyplot.show()
         #Print out predicted number
         #print("This is a {}".format(label))
         #print("Certainty: {}".format(certainty))
